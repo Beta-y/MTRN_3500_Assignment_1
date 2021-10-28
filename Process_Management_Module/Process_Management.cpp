811c64a2dc9a6827346b80641b5b8b52c4edf4b5
Beta-y <58842921+Beta-y@users.noreply.github.com>
2021-10-28 09:51:01 +0800

    GPS Heartbeats Debug

--- Process_Management_Module/Process_Management.cpp
+++ Process_Management_Module/Process_Management.cpp
@@ -44,18 +44,18 @@ int main()
 	// Do not shut down any processes at the startup of the program
 	//ProcessManagementPtr->Shutdown.Flags.Camera = 0b0;
 	//ProcessManagementPtr->Shutdown.Flags.GPS = 0b0;
-	ProcessManagementPtr->Shutdown.Flags.Laser = 0b0;
+	//ProcessManagementPtr->Shutdown.Flags.Laser = 0b0;
 /*	ProcessManagementPtr->Shutdown.Flags.OpenGL = 0b0;
 	ProcessManagementPtr->Shutdown.Flags.ProcessManagement = 0b0;
 	ProcessManagementPtr->Shutdown.Flags.VehicleControl = 0b0*/;
 
-	//SMObject SM_GPSObj(_TEXT("SM_GPS"), sizeof(SM_GPS));
-	//SM_GPSObj.SMCreate();
-	//SM_GPSObj.SMAccess();
+	SMObject SM_GPSObj(_TEXT("SM_GPS"), sizeof(SM_GPS));
+	SM_GPSObj.SMCreate();
+	SM_GPSObj.SMAccess();
 
-	SMObject SM_LaserObj(_TEXT("SM_Laser"), sizeof(SM_Laser));
-	SM_LaserObj.SMCreate();
-	SM_LaserObj.SMAccess();
+	//SMObject SM_LaserObj(_TEXT("SM_Laser"), sizeof(SM_Laser));
+	//SM_LaserObj.SMCreate();
+	//SM_LaserObj.SMAccess();
 
 	//SMObject SM_VehicleControlObj(_TEXT("SM_VehicleControl"), sizeof(SM_VehicleControl));
 	//SM_VehicleControlObj.SMCreate();
@@ -63,13 +63,24 @@ int main()
 
 
 	//start all 5 modules
-	ProcessManagementPtr->Heartbeat.Flags.Laser = 0b1;
-	ProcessManagementPtr->Shutdown.Flags.Laser = 0b0;
+	//ProcessManagementPtr->Heartbeat.Flags.Laser = 0b1;
+	//ProcessManagementPtr->Shutdown.Flags.Laser = 0b0;
+	// 
+	//ProcessManagementPtr->Heartbeat.Flags.Camera = 0b1;
+	//ProcessManagementPtr->Shutdown.Flags.Camera = 0b0;
+
+	ProcessManagementPtr->Heartbeat.Flags.GPS = 0b1;
+	ProcessManagementPtr->Shutdown.Flags.GPS = 0b0;
 
 	STARTUPINFO startupinfos[NUM_UNITS];//Specifies appearance of the main window etc. for a process at creation time.
 	PROCESS_INFORMATION processinfos[NUM_UNITS];//Receives identification information about the new process.
 	
-	StartProcess(Units[2], &startupinfos[2], &processinfos[2]);
+	/*StartProcess(Units[2], &startupinfos[2], &processinfos[2]);*/
+	/*StartProcess(Units[0], &startupinfos[0], &processinfos[0]);*/
+
+	Sleep(10);// Wait for 10ms to allow SM to fully start up
+
+	StartProcess(Units[1], &startupinfos[1], &processinfos[1]);
 	
 	
 	//for (int i = 0; i < NUM_UNITS; i++)
@@ -85,11 +96,11 @@ int main()
 
 	Sleep(1000);// Wait for 1s to allow processes to fully start up
 
-	int cnt_Laser = 0;
-	//int cnt_GPS = 0;
+	//int cnt_Laser = 0;
+	int cnt_GPS = 0;
 	//int cnt_OpenGL = 0;
 	//int cnt_VehicleControl = 0;
-	//int cnt_Camera = 0;
+	/*int cnt_Camera = 0;*/
 	int MAX_WAITBEAT = 100;
 	// main loop
 	while (1)
@@ -98,112 +109,143 @@ int main()
 		if ((_kbhit() && _getch() == 27) || ProcessManagementPtr->Shutdown.Flags.ProcessManagement == 0b1) break; //You can set it to 0b1 in other module's exceptions to shutdown the whole PM process, which means it's a cross-level operation.
 
 		/* Heartbear check, need simplification*/
-		if (ProcessManagementPtr->Shutdown.Flags.Laser == 0b1)
-		{
-			KillProcess(Units[2], processinfos[2].hProcess);
-			StartProcess(Units[2], &startupinfos[2], &processinfos[2]);
-		}
-		else if (ProcessManagementPtr->Heartbeat.Flags.Laser == 0b1) // Heartbeat still
-		{
-			cout << "Laser is normal: 1, count=" << cnt_Laser << endl;
-			ProcessManagementPtr->Heartbeat.Flags.Laser = 0b0; // Clear for next heartbeat checking
-			cnt_Laser = 0;
-		}
-		else if (ProcessManagementPtr->Heartbeat.Flags.Laser == 0b0) // Heartbeat stop
-		{
-			cout << "Laser no response: 0, count=" << cnt_Laser << endl;
-			cnt_Laser++;
-			if (cnt_Laser > MAX_WAITBEAT)
-			{
-				ProcessManagementPtr->Shutdown.Status = 0xFF; // Critical laser module is broken, all modules need to be shutdown.
-				std::cout << "Critical laser module is broken." << std::endl;
-				continue;
-			}
-		}
-		Sleep(100);
-/*		if (ProcessManagementPtr->Shutdown.Flags.Camera == 0b1)
-		{
-			KillProcess(Units[0], processinfos[0].hProcess);
-			StartProcess(Units[0], &startupinfos[0], &processinfos[0]);
-		}
-		else if (ProcessManagementPtr->Heartbeat.Flags.Camera == 0b1)
-		{
-			ProcessManagementPtr->Heartbeat.Flags.Camera = 0b0;
-			cnt_Camera = 0;
-		}
-		else if (ProcessManagementPtr->Heartbeat.Flags.Camera == 0b0)
-		{
-			cnt_Camera++;
-			if (cnt_Camera > MAX_WAITBEAT)
-			{
-				ProcessManagementPtr->Shutdown.Status = 0xFF;
-				std::cout << "Critical camera module is broken." << std::endl;
-				continue;
-			}
-		}
+		//if (ProcessManagementPtr->Shutdown.Flags.Laser == 0b1)
+		//{
+		//	KillProcess(Units[2], processinfos[2].hProcess);
+		//	StartProcess(Units[2], &startupinfos[2], &processinfos[2]);
+		//}
+		//else if (ProcessManagementPtr->Heartbeat.Flags.Laser == 0b1) // Heartbeat still
+		//{
+		//	cout << "Laser is normal: 1, count=" << cnt_Laser << endl;
+		//	ProcessManagementPtr->Heartbeat.Flags.Laser = 0b0; // Clear for next heartbeat checking
+		//	cnt_Laser = 0;
+		//}
+		//else if (ProcessManagementPtr->Heartbeat.Flags.Laser == 0b0) // Heartbeat stop
+		//{
+		//	cout << "Laser no response: 0, count=" << cnt_Laser << endl;
+		//	cnt_Laser++;
+		//	if (cnt_Laser > MAX_WAITBEAT)
+		//	{
+		//		ProcessManagementPtr->Shutdown.Status = 0xFF; // Critical laser module is broken, all modules need to be shutdown.
+		//		std::cout << "Critical laser module is broken." << std::endl;
+		//		continue;
+		//	}
+		//}
+		//Sleep(100);
 
+		//if (ProcessManagementPtr->Shutdown.Flags.Camera == 0b1)
+		//{
+		//	KillProcess(Units[0], processinfos[0].hProcess);
+		//	StartProcess(Units[0], &startupinfos[0], &processinfos[0]);
+		//	ProcessManagementPtr->Heartbeat.Flags.Camera = 0b0;
+		//	ProcessManagementPtr->Shutdown.Flags.Camera = 0b0;
+		//}
+		//else if (ProcessManagementPtr->Heartbeat.Flags.Camera == 0b1) // Heartbeat still
+		//{
+		//	cout << "Camera is normal: 1, count=" << cnt_Camera << endl;
+		//	ProcessManagementPtr->Heartbeat.Flags.Camera = 0b0; // Clear for next heartbeat checking
+		//	cnt_Camera = 0;
+		//}
+		//else if (ProcessManagementPtr->Heartbeat.Flags.Camera == 0b0) // Heartbeat stop
+		//{
+		//	cout << "Camera no response: 0, count=" << cnt_Camera << endl;
+		//	cnt_Camera++;
+		//	if (cnt_Camera > MAX_WAITBEAT)
+		//	{
+		//		ProcessManagementPtr->Shutdown.Status = 0xFF; // Critical laser module is broken, all modules need to be shutdown.
+		//		std::cout << "Critical camera module is broken." << std::endl;
+		//		continue;
+		//	}
+		//}
+		//Sleep(100);
+		
 		if (ProcessManagementPtr->Shutdown.Flags.GPS == 0b1)
 		{
 			KillProcess(Units[1], processinfos[1].hProcess);
 			StartProcess(Units[1], &startupinfos[1], &processinfos[1]);
-		}
-		else if (ProcessManagementPtr->Heartbeat.Flags.GPS == 0b1)
-		{
 			ProcessManagementPtr->Heartbeat.Flags.GPS = 0b0;
+			ProcessManagementPtr->Shutdown.Flags.GPS = 0b0;
+		}
+		else if (ProcessManagementPtr->Heartbeat.Flags.GPS == 0b1) // Heartbeat still
+		{
+			cout << "GPS is normal: 1, count=" << cnt_GPS << endl;
+			ProcessManagementPtr->Heartbeat.Flags.GPS = 0b0; // Clear for next heartbeat checking
 			cnt_GPS = 0;
 		}
-		else if (ProcessManagementPtr->Heartbeat.Flags.GPS == 0b0)
+		else if (ProcessManagementPtr->Heartbeat.Flags.GPS == 0b0) // Heartbeat stop
 		{
+			cout << "GPS no response: 0, count=" << cnt_GPS << endl;
 			cnt_GPS++;
 			if (cnt_GPS > MAX_WAITBEAT)
 			{
-				ProcessManagementPtr->Shutdown.Status = 0xFF;
+				//ProcessManagementPtr->Shutdown.Status = 0xFF; // Critical laser module is broken, all modules need to be shutdown.
 				std::cout << "Critical GPS module is broken." << std::endl;
-			}
-		}
-
-		if (ProcessManagementPtr->Shutdown.Flags.OpenGL == 0b1)
-		{
-			KillProcess(Units[3], processinfos[3].hProcess);
-			StartProcess(Units[3], &startupinfos[3], &processinfos[3]);
-		}
-		else if (ProcessManagementPtr->Heartbeat.Flags.OpenGL == 0b1)
-		{
-			ProcessManagementPtr->Heartbeat.Flags.OpenGL = 0b0;
-			cnt_OpenGL = 0;
-		}
-		else if (ProcessManagementPtr->Heartbeat.Flags.OpenGL == 0b0)
-		{
-			cnt_OpenGL++;
-			if (cnt_OpenGL > MAX_WAITBEAT)
-			{
-				ProcessManagementPtr->Shutdown.Status = 0xFF;
-				std::cout << "Critical OpenGL module is broken." << std::endl;
 				continue;
 			}
 		}
+		Sleep(100);
 
-		if (ProcessManagementPtr->Shutdown.Flags.VehicleControl == 0b1)
-		{
-			KillProcess(Units[4], processinfos[4].hProcess);
-			StartProcess(Units[4], &startupinfos[4], &processinfos[4]);
-		}
-		else if (ProcessManagementPtr->Heartbeat.Flags.VehicleControl == 0b1)
-		{
-			ProcessManagementPtr->Heartbeat.Flags.VehicleControl = 0b0;
-			cnt_VehicleControl = 0;
-		}
-		else if (ProcessManagementPtr->Heartbeat.Flags.VehicleControl == 0b0)
-		{
-			cnt_VehicleControl++;
-			if (cnt_VehicleControl > MAX_WAITBEAT)
-			{
-				ProcessManagementPtr->Shutdown.Status = 0xFF;
-				std::cout << "Critical VehicleControl module is broken." << std::endl;
-				continue;
-			}
-		}
-	*/
+		//if (ProcessManagementPtr->Shutdown.Flags.GPS == 0b1)
+		//{
+		//	KillProcess(Units[1], processinfos[1].hProcess);
+		//	StartProcess(Units[1], &startupinfos[1], &processinfos[1]);
+		//}
+		//else if (ProcessManagementPtr->Heartbeat.Flags.GPS == 0b1)
+		//{
+		//	ProcessManagementPtr->Heartbeat.Flags.GPS = 0b0;
+		//	cnt_GPS = 0;
+		//}
+		//else if (ProcessManagementPtr->Heartbeat.Flags.GPS == 0b0)
+		//{
+		//	cnt_GPS++;
+		//	if (cnt_GPS > MAX_WAITBEAT)
+		//	{
+		//		ProcessManagementPtr->Shutdown.Status = 0xFF;
+		//		std::cout << "Critical GPS module is broken." << std::endl;
+		//	}
+		//}
+
+		//if (ProcessManagementPtr->Shutdown.Flags.OpenGL == 0b1)
+		//{
+		//	KillProcess(Units[3], processinfos[3].hProcess);
+		//	StartProcess(Units[3], &startupinfos[3], &processinfos[3]);
+		//}
+		//else if (ProcessManagementPtr->Heartbeat.Flags.OpenGL == 0b1)
+		//{
+		//	ProcessManagementPtr->Heartbeat.Flags.OpenGL = 0b0;
+		//	cnt_OpenGL = 0;
+		//}
+		//else if (ProcessManagementPtr->Heartbeat.Flags.OpenGL == 0b0)
+		//{
+		//	cnt_OpenGL++;
+		//	if (cnt_OpenGL > MAX_WAITBEAT)
+		//	{
+		//		ProcessManagementPtr->Shutdown.Status = 0xFF;
+		//		std::cout << "Critical OpenGL module is broken." << std::endl;
+		//		continue;
+		//	}
+		//}
+
+		//if (ProcessManagementPtr->Shutdown.Flags.VehicleControl == 0b1)
+		//{
+		//	KillProcess(Units[4], processinfos[4].hProcess);
+		//	StartProcess(Units[4], &startupinfos[4], &processinfos[4]);
+		//}
+		//else if (ProcessManagementPtr->Heartbeat.Flags.VehicleControl == 0b1)
+		//{
+		//	ProcessManagementPtr->Heartbeat.Flags.VehicleControl = 0b0;
+		//	cnt_VehicleControl = 0;
+		//}
+		//else if (ProcessManagementPtr->Heartbeat.Flags.VehicleControl == 0b0)
+		//{
+		//	cnt_VehicleControl++;
+		//	if (cnt_VehicleControl > MAX_WAITBEAT)
+		//	{
+		//		ProcessManagementPtr->Shutdown.Status = 0xFF;
+		//		std::cout << "Critical VehicleControl module is broken." << std::endl;
+		//		continue;
+		//	}
+		//}
 	}
 
 	//kill all 5 modules
@@ -212,7 +254,7 @@ int main()
 	//	KillProcess(Units[i], processinfos[i].hProcess);
 	//}
 
-	KillProcess(Units[2], processinfos[2].hProcess);
+	KillProcess(Units[1], processinfos[1].hProcess);
 	// kill PM module
 	system("pause");
 	return 0;
